24b27579913c076da481c50e07b8c471e9334088
amrazek <amrazek@csu.fullerton.edu>
2019-09-23 04:36:31 -0700

    added ship movement limits

--- game_functions.py
+++ game_functions.py
@@ -2,6 +2,21 @@ import sys
 import pygame
 
 
+def check_keydown_events(event, ship):
+    """Respond to keypresses."""
+    if event.key == pygame.K_RIGHT:
+        # move the ship to the right
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     """Respond to keyboard and mouse events"""
     # watch for keyboard and mouse events
@@ -10,17 +25,11 @@ def check_events(ship):
             pygame.quit()
             sys.exit()
         elif evt.type == pygame.KEYDOWN:
-            if evt.key == pygame.K_RIGHT:
-                # move the ship to the right
-                ship.moving_right = True
-            elif evt.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(evt, ship)
 
         elif evt.type == pygame.KEYUP:
-            if evt.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif evt.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(evt, ship)
+
 
 def update_screen(ai_settings, screen, ship):
     # redraw screen
