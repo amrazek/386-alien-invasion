8ae4d7e015c9ee9588ceadceb4852523293d31fc
amrazek <amrazek@csu.fullerton.edu>
2019-09-23 05:30:53 -0700

    added game_stats; player lives

--- settings.py
+++ settings.py
@@ -11,9 +11,10 @@ class Settings:
 
         # ship settings
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
 
         # bullet settings
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 2
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
